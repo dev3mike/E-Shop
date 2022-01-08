8d15428a5c6c1a205236c119363d2f2828ceb1cb
Masoud Banimahd <masoud@eliq.io>
2022-01-08 13:33:22 +0100

    product entity has been created

---
+++ src/Services/Inventory/EShop.Inventory.API/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace EShop.Inventory.API.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("category")]
+        public string Category { get; set; }
+
+        [BsonElement("summary")]
+        public string Summary { get; set; }
+
+        [BsonElement("description")]
+        public string Description { get; set; }
+
+        [BsonElement("image_file")]
+        public string Image { get; set; }
+
+        [BsonElement("price")]
+        public decimal Price { get; set; }
+    }
+}
