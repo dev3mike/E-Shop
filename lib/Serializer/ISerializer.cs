d36a37b9b55c5c26750d81da1d179ac8fe550cb9
Masoud Banimahd <masoud@eliq.io>
2022-02-02 19:22:10 +0100

    Serializer library project has been added

---
+++ lib/Serializer/ISerializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Libraries.Serializer
+{
+    public interface ISerializer
+    {
+        string SerializeObject(object o);
+        T DeserializeObject<T>(string json);
+    }
+}
---
+++ lib/Serializer/Serializer.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Eshop.Libraries.Serializer
+{
+    public class Serializer : ISerializer
+    {
+        public T DeserializeObject<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        public string SerializeObject(object o)
+        {
+            return JsonConvert.SerializeObject(o);
+        }
+    }
+}
