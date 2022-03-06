c3dea5732cb476dacfd20b660ad5ac69e4fffe09
Masoud Banimahd <masoud@eliq.io>
2022-03-06 20:40:16 +0100

    IVoucherRepository has been implemented

---
+++ src/Services/Voucher/EShop.Voucher.API/Repositories/IVoucherRepository.cs
@@ -0,0 +1,13 @@
+﻿using EShop.Voucher.API.Entities;
+using System.Threading.Tasks;
+
+namespace EShop.Voucher.API.Repositories
+{
+    public interface IVoucherRepository
+    {
+        EVoucher GetVoucher(string voucherCode);
+        EVoucher CreateVoucher(EVoucher eVoucher);
+        EVoucher UpdateVoucher(EVoucher eVoucher);
+        bool DeleteVoucher(EVoucher eVoucher);
+    }
+}
---
+++ src/Services/Voucher/EShop.Voucher.API/Repositories/VoucherRepository.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using Dapper.Contrib.Extensions;
+using EShop.Voucher.API.Entities;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Threading.Tasks;
+
+namespace EShop.Voucher.API.Repositories
+{
+    public class VoucherRepository : IVoucherRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public VoucherRepository(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public EVoucher CreateVoucher(EVoucher eVoucher)
+        {
+            var connection = GetPostgresConnection();
+            var result = connection.Insert(eVoucher);
+            eVoucher.Id = result;
+            return eVoucher;
+        }
+
+        public bool DeleteVoucher(EVoucher eVoucher)
+        {
+            var connection = GetPostgresConnection();
+            var isSuccessful = connection.Delete(eVoucher);
+            return isSuccessful;
+        }
+
+        public EVoucher GetVoucher(string voucherCode)
+        {
+            var connection = GetPostgresConnection();
+            var voucher = connection.QueryFirstOrDefault<EVoucher>(
+                sql: "SELECT * FROM Voucher WHERE Code=@VoucherCode", 
+                param: new { VoucherCode = voucherCode});
+            return voucher;
+        }
+
+        public EVoucher UpdateVoucher(EVoucher eVoucher)
+        {
+            var connection = GetPostgresConnection();
+            var isSuccessful = connection.Update(eVoucher);
+            return isSuccessful ? eVoucher : null;
+        }
+
+        private NpgsqlConnection GetPostgresConnection()
+        {
+            using var connection = new NpgsqlConnection(
+                _configuration.GetValue<string>("DatabaseSettings:ConnectionString")
+                );
+            return connection;
+        }
+    }
+}
