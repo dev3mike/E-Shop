09e98cda4151f2d7a3aaa9a9a2195eb97d73740d
Masoud Banimahd <masoud@eliq.io>
2022-03-06 20:56:08 +0100

    Voucher controller has been implemented

---
+++ src/Services/Voucher/EShop.Voucher.API/Controllers/VoucherController.cs
@@ -0,0 +1,65 @@
+﻿using EShop.Voucher.API.Entities;
+using EShop.Voucher.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace EShop.Voucher.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class VoucherController : ControllerBase
+    {
+        private readonly IVoucherRepository _voucherRepository;
+
+        public VoucherController(IVoucherRepository voucherRepository)
+        {
+            _voucherRepository = voucherRepository;
+        }
+
+        public ActionResult<EVoucher> GetVoucher(string voucherCode)
+        {
+            var voucher = _voucherRepository.GetVoucher(voucherCode);
+            return Ok(voucher);
+        }
+
+        public ActionResult<EVoucher> CreateVoucher(EVoucher eVoucher)
+        {
+            ValidateEVoucher(eVoucher);
+
+            var voucher = _voucherRepository.CreateVoucher(eVoucher);
+            if (voucher == null)
+                throw new Exception("Could not save the voucher");
+
+            return Ok(voucher);
+        }
+
+        public ActionResult<EVoucher> UpdateVoucher(EVoucher eVoucher)
+        {
+            ValidateEVoucher(eVoucher);
+
+            var voucher = _voucherRepository.UpdateVoucher(eVoucher);
+            return Ok(voucher);
+        }
+
+        public ActionResult DeleteVoucher(string voucherCode)
+        {
+            var voucher = _voucherRepository.GetVoucher(voucherCode);
+            if (voucher == null) return NotFound();
+
+            var isDeleted = _voucherRepository.DeleteVoucher(voucher);
+            return isDeleted ? NoContent() : StatusCode(500);
+        }
+
+        private void ValidateEVoucher(EVoucher eVoucher)
+        {
+            if (
+                eVoucher?.Id == null ||
+                eVoucher?.Code == null ||
+                eVoucher?.Amount == null
+                )
+                throw new BadHttpRequestException("eVoucher is not valid", 400);
+        }
+    }
+}
